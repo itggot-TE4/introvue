38c86ae9940abf0c5c135e2851553b177891f307
Linus Styrén <linus.styren@ga.ntig.se>
2020-11-10 21:44:46 +0100

    add TodoItem component

---
+++ todos2/src/components/TodoItem.vue
@@ -0,0 +1,16 @@
+<template>
+  <li>
+     {{title}}
+  </li>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  data() {
+     return {
+        title: "Hello, World!"
+     }
+  }
+};
+</script>
