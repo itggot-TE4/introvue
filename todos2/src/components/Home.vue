194c79deba818116dabd4733dea533a841f5e8ac
Linus Styrén <linus.styren@ga.ntig.se>
2020-11-10 22:09:50 +0100

    add itemform component

--- todos2/src/components/Home.vue
+++ todos2/src/components/Home.vue
@@ -1,32 +1,37 @@
 <template>
   <div class="home">
-      <TodoItem v-for="todoItem in todoItems"
-                :key="todoItem.id"
-                :todoItem="todoItem" />
+    <TodoItem
+      v-for="todoItem in todoItems"
+      :key="todoItem.id"
+      :todoItem="todoItem"
+    />
+    <TodoForm />
   </div>
 </template>
 
 <script>
 import TodoItem from "./TodoItem.vue";
+import TodoForm from "./TodoForm.vue";
 
 export default {
   name: "Home",
   data() {
-      return {
-          todoItems: [
-              {
-                  id: 0,
-                  title: 'write code'
-              },
-              {
-                  id: 1,
-                  title: 'clean my kitchen...'
-              }
-          ]
-      }
+    return {
+      todoItems: [
+        {
+          id: 0,
+          title: "write code"
+        },
+        {
+          id: 1,
+          title: "clean my kitchen..."
+        }
+      ]
+    };
   },
   components: {
-      TodoItem
+    TodoItem,
+    TodoForm
   }
 };
 </script>
---
+++ todos2/src/components/TodoForm.vue
@@ -0,0 +1,17 @@
+<template>
+   <form> 
+      <input type="text" name="title">
+      <button>Create Todo</button>
+   </form>
+</template>
+
+<script>
+export default {
+  name: "TodoForm",
+  data() {
+     return {
+        title: ""
+     }
+  }
+};
+</script>
