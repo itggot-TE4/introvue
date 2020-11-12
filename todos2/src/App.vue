5534c77c9dfc844207b85d18e06e2ee91e8ebcc1
Linus Styrén <linus.styren@ga.ntig.se>
2020-11-13 00:51:34 +0100

    don't show home as option

--- todos2/src/App.vue
+++ todos2/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
     <router-link to="/todos">Todos</router-link> |
     <router-link to="/about">About</router-link> |
     <router-link to="/login">Login</router-link>
