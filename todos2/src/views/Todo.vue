1786a847e36d7ad47b0d7d9b3fca6fa826855e5a
Jonathan Ronsten <jonathan.ronsten@elev.ga.ntig.se>
2020-11-13 12:43:46 +0100

    Added functionality you wanted

--- todos2/src/views/Todo.vue
+++ todos2/src/views/Todo.vue
@@ -1,15 +1,57 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
+    <div>
+      {{ todo.title }}
+    </div>
+    <input type="text" v-model="todo.title" />
+    <textarea v-model="todo.description" />
+    <button @click="updateTodo">Update Todo</button>
   </div>
 </template>
 
 <script>
 export default {
   name: "Todo",
+  data() {
+    return {
+      todo: {
+        title: "",
+        description: "",
+        isCompleted: 0
+      }
+    };
+  },
+  methods: {
+    updateTodo() {
+      this.todo = {
+        title: this.todo.title,
+        description: this.todo.description,
+        isCompleted: this.todo.isCompleted
+      }
+      fetch(`http://localhost:9292/api/v1/todos/${this.$route.params.id}`, {
+        method: "PATCH",
+        headers: {
+          "content-type": "application/json"
+        },
+        body: JSON.stringify(this.todo)
+      })
+    }
+  },
   mounted() {
+    console.log('making request')
+    fetch(`http://localhost:9292/api/v1/todos/${this.$route.params.id}`, {
+      method: "get",
+      headers: {
+        "content-type": "application/json"
+      }
+    }).then(res => {
+        console.log(res)
+        return res.json();
+      })
+      .then(json => (this.todo = json)).catch( error=> console.log(error))  
+
     // if logged in
-    console.log(this.$route.params);
+    // console.log(this.$route.params);
   }
 };
 </script>
