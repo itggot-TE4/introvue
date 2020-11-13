207af370b3178f673944669aa61a97a7236d1856
adr29truck <adrianalmetunsmeds@gmail.com>
2020-11-13 12:39:44 +0100

    Allow todoForm to be dynamically genereated

--- todos2/src/components/TodoForm.vue
+++ todos2/src/components/TodoForm.vue
@@ -1,22 +1,53 @@
 <template>
   <form>
-    <input v-model="title" type="text" name="title" />
-    <button @click.prevent="createTodo">Create Todo</button>
+    <input v-model="title" type="text" name="title" placeholder="title"/>
+    <input
+      v-model="description"
+      type="text"
+      name="description"
+      placeholder="description"
+    />
+    <button @click.prevent="createTodo">{{ text }}</button>
   </form>
 </template>
 
 <script>
+import { watch, ref } from "vue";
+
 export default {
   name: "TodoForm",
-  data() {
-    return {
-      title: ""
-    };
+  props: {
+    todo: {
+      type: Object,
+      default: () => {
+        return { title: "" };
+      }
+    },
+    text: {
+      type: String,
+      default: "Create Todo"
+    }
   },
   methods: {
     createTodo() {
-      this.$emit("add-todo", this.title);
+      this.$emit("add-todo", {
+        title: this.title,
+        description: this.description
+      });
     }
+  },
+  setup(props) {
+    const title = ref(null);
+    const description = ref(null);
+
+    watch(
+      () => props.todo,
+      async (newVal, oldVal) => {
+        title.value = newVal.title;
+        description.value = newVal.description;
+      }
+    );
+    return { title, description };
   }
 };
 </script>
