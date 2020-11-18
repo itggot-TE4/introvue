4a89d63974f5ec5b6a58228f10a03ba7a2bda31c
Linus Styrén <linus.styren@ga.ntig.se>
2020-11-18 09:48:58 +0100

    add persistent state

--- todos2/src/store/index.ts
+++ todos2/src/store/index.ts
@@ -1,9 +1,13 @@
 import { createStore } from "vuex";
+import createPersistedState from "vuex-persistedstate";
 import todos from "./modules/todos";
 import auth from "./modules/auth";
 import user from "./modules/user";
 
 export default createStore<{}>({
   strict: true,
-  modules: { todos, auth, user }
+  modules: { todos, auth, user },
+  plugins: [createPersistedState({
+    paths: ['user']
+  })]
 });
