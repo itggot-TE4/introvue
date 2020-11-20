d574653a95b01ebeb654caacb24d2ecb84c708be
Linus Styrén <linus.styren@ga.ntig.se>
2020-11-20 11:09:54 +0100

    added vuetify

---
+++ vuetify/src/App.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn dark @click="snackbar = true"> Open Snackbar </v-btn>
+      <v-snackbar v-model="snackbar">
+        {{ text }}
+
+        <template v-slot:action="{ attrs }">
+          <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+            Close
+          </v-btn>
+        </template>
+      </v-snackbar>
+
+      <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <HelloWorld />
+      <v-progress-circular indeterminate></v-progress-circular>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default Vue.extend({
+  name: "App",
+
+  components: {
+    HelloWorld,
+  },
+  mounted() {
+    setInterval(() => {
+      this.loadingProgress += 20;
+      if (this.loadingProgress > 100) this.loadingProgress = 0;
+    }, 500)
+  },
+  data: () => ({
+    snackbar: false,
+    text: "Hello World!!!!!!",
+    loadingProgress: 0
+  }),
+});
+</script>
